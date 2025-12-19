39fdf225a23c06eb8e1191db873669c89efb5e86
bishalpaul777 <paulbishal536@gmail.com>
2025-12-19 21:56:17 +0530

    master file updated

--- pyspark/master.py
+++ pyspark/master.py
@@ -11,26 +11,39 @@ os.environ['HADOOP_HOME'] = r"D:\hadoop"
 os.environ['PATH'] = os.environ['PATH'] + r";D:\hadoop\bin"
 os.environ['JAVA_HOME'] = r"C:\Users\HP\.jdks\corretto-1.8.0_462"
 
+from pyspark.sql import SparkSession
+
 spark = (
     SparkSession.builder
     .appName("masterJob")
     .getOrCreate()
 )
 
-
-sc = spark.sparkContext
 spark.sparkContext.setLogLevel("ERROR")
 
-#=======================================================================
+# =====================================================================
 
-s3data = spark.read.load("s3://spark-jobs1/dest/s3Output/")
-snowdata = spark.read.load("s3://spark-jobs1/dest/snowOutput/part-00000-c88834f0-35bc-4760-bf3c-364b6c46a16d-c000.snappy.parquet")
+s3data = spark.read.parquet("s3://spark-jobs1/dest/s3Output/")
+snowdata = spark.read.parquet(
+    "s3://spark-jobs1/dest/snowOutput/part-00000-c88834f0-35bc-4760-bf3c-364b6c46a16d-c000.snappy.parquet"
+)
 
 s3data.createOrReplaceTempView("sdata")
 snowdata.createOrReplaceTempView("snow")
 
+result = spark.sql("""
+    SELECT DISTINCT
+        sn.name,
+        sn.city,
+        s.department,
+        s.project_name,
+        s.salary
+    FROM snow sn
+    INNER JOIN sdata s
+    ON sn.city = s.city
+""")
 
-result = spark.sql(" select distinct sn.name,sn.city,s.department,s.project_name,s.salary from snow sn inner join sdata s on sn.city = s.city")
-result.show(result.count(), truncate=False)
+result.show(truncate=False)
+
 
 
